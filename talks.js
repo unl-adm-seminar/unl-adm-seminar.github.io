399c5ea539f64296300aa21647a50ec899fd0a21
shiyinglive <53949026+shiyinglive@users.noreply.github.com>
2026-09-15 17:59:34 -0500

    Update talks.js

--- talks.js
+++ talks.js
@@ -152,8 +152,7 @@ This talk reports on joint work with Rainey Lyons (University of Colorado Boulde
     // title and abstract to come
   },
   { date: "2026-11-26", noSeminar: true, reason: "No seminar — Thanksgiving" },
-
-   {
+ {
     date: "2026-12-03",
     speaker: "Chuxiangbo Wang",
     affiliation: "University of North Carolina at Chapel Hill",
